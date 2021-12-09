8a805835be7ecf3446d82bcfc110333685f2277a
DeveNinja <abbays10@gmail.com>
2021-12-09 21:56:49 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import { next, prev } from './actions/GraphActions';
+import { useDispatch } from 'react-redux';
+
+function App() {
+
+  const dispatch = useDispatch()
+
+  return (
+    <div className="App" style={{marginTop: '40%'}}>
+      <button onClick={ () => dispatch(next()) }>Next</button>
+      <br />
+      <button onClick={ () => dispatch(prev()) }>Prev</button>
+    </div>
+  );
+}
+
+export default App;
